57d36565c601f67351a22dadb3c918607a8c6c02
Alexis Leriche <a.leriche@arcadebeauty.eu>
2020-01-22 17:19:31 +0100

    Ajout des commentaire

--- BackupAndRestor.py
+++ BackupAndRestor.py
@@ -26,35 +26,35 @@ def backup():
 
         try:
                 os.stat(PATHBACKUPTMP)
-                print('The folder ' + PATHBACKUPTMP +' exist')
+                print('The folder ' + PATHBACKUPTMP +' exist')    #######   test si le dossier  contenu dans la variable PATHBACKUPTMP existe   #######
         except:
                 os.mkdir(PATHBACKUPTMP)
-                print('Create folder ' + PATHBACKUPTMP)
+                print('Create folder ' + PATHBACKUPTMP)           #######   sinon il le creer   #######
         try:
                 os.stat(PATHBACKUP)
-                print('The folder ' + PATHBACKUP +' exist')
+                print('The folder ' + PATHBACKUP +' exist')       #######   test si le dossier contenu dans PATHBACKUP existe   #######
         except:
                 os.mkdir(PATHBACKUP)
-                print('Create folder ' + PATHBACKUP)
+                print('Create folder ' + PATHBACKUP)              #######   sinon il le creer   #######
 
         print('Start Backup of databases ' + DB_NAME+ '...')
-        dumpcmd = "mysqldump -h " + DB_HOST + " -u " + DB_USER + " -p" + DB_PASSWORD + " " + DB_NAME + " > " + PATHBACKUPTMP + "/" + DB_NAME + ".sql"
+        dumpcmd = "mysqldump -h " + DB_HOST + " -u " + DB_USER + " -p" + DB_PASSWORD + " " + DB_NAME + " > " + PATHBACKUPTMP + "/" + DB_NAME + ".sql"   #######   Création de la sauvegarde de la bases de donnée    #######
         os.system(dumpcmd)
 
         print('copy the folder ' + WP_PATH + '...')
-        os.system('cp -r' + WP_PATH + ' '+ PATHBACKUPTMP)
+        os.system('cp -r' + WP_PATH + ' '+ PATHBACKUPTMP)       #######   Copie du repertoire Wordpress   #######
 
         print('copy the folder ' + APACHE_PATH +'...')
-        os.system('cp -r' + APACHE_PATH + ' '+ PATHBACKUPTMP)
+        os.system('cp -r' + APACHE_PATH + ' '+ PATHBACKUPTMP)   #######   Copie du repertoire Apache   #######
 
         print('ZIP the folder ' + PATHBACKUPTMP + 'to ' + PATHBACKUP )
-        os.system('tar -czvf ' + PATHBACKUP +'FullBackup-'+DATETIME+'.tar.gz ' + PATHBACKUPTMP)
+        os.system('tar -czvf ' + PATHBACKUP +'FullBackup-'+DATETIME+'.tar.gz ' + PATHBACKUPTMP)         #######   Création de l'archive   #######
 
         print(' Create the link')
-        os.system('rm -rf '+ PATHBACKUP + 'LastBackup.tar.gz' )
-        os.system('ln -s ' + PATHBACKUP +'FullBackup-'+DATETIME+'.tar.gz ' + PATHBACKUP +'/LastBackup.tar.gz' )
+        os.system('rm -rf '+ PATHBACKUP + 'LastBackup.tar.gz' )         #######   Supression de l'ancien lien symbolique    #######
+        os.system('ln -s ' + PATHBACKUP +'FullBackup-'+DATETIME+'.tar.gz ' + PATHBACKUP +'/LastBackup.tar.gz' )         #######   création du lien symbolique    #######
 
-        os.system('rm -rf '+ PATHBACKUPTMP)
+        os.system('rm -rf '+ PATHBACKUPTMP)     #######   Supression du repertoire temporaire    #######
 
         print('Backup databases + Wordpress + Apache is done')
 
@@ -62,27 +62,27 @@ def backup():
 def restore():
 
         try:
-                os.stat(PATHRESTORETMP)
-                print('The folder ' + PATHRESTORETMP +' exist')
+                os.stat(PATHRESTORETMP)                                 #######   test si le dossier  contenu dans la variable PATHRESTORETMP existe   #######
+                print('The folder ' + PATHRESTORETMP +' exist')          
         except:
-                os.mkdir(PATHRESTORETMP)
+                os.mkdir(PATHRESTORETMP)                                #######   sinon il le creer   #######
                 print('Create folder ' + PATHRESTORETMP)
 
         print('UNZip the folder ' + LASTBACKUP + ' to ' + PATHRESTORETMP )
-        os.system('tar -xzvf '+ LASTBACKUP + ' -C ' + PATHRESTORETMP )
+        os.system('tar -xzvf '+ LASTBACKUP + ' -C ' + PATHRESTORETMP )          #######   extraction del'archive   #######
 
         print('copy the folder to ' + WP_PATH)
-        os.system('cp -r' + PATHRESTORE + 'www /var/')
+        os.system('cp -r' + PATHRESTORE + 'www /var/')                  #######   Copie du dossier www vers /var/   #######
 
         print('copy the folder to ' + APACHE_PATH)
-        os.system('cp -r' + PATHRESTORE + 'apache2 /etc/')
+        os.system('cp -r' + PATHRESTORE + 'apache2 /etc/')              #######   Copie du dossier Apache2 vers /etc/   #######
 
         print('Start Restore of databases ' + DB_NAME+ '...')
-        os.system("mysql -h " + DB_HOST + " -u " + DB_USER + " -p" + DB_PASSWORD + " " + DB_NAME + " < " + PATHRESTORE + "/" + DB_NAME + ".sql")
+        os.system("mysql -h " + DB_HOST + " -u " + DB_USER + " -p" + DB_PASSWORD + " " + DB_NAME + " < " + PATHRESTORE + "/" + DB_NAME + ".sql")        #######   Restauration de la bases de donnée    #######
         
         print('Remove /tmp')
-        os.system('rm -rf '+ PATHRESTORETMP)
-        os.system('rm -rf '+ PATHRESTORE)
+        os.system('rm -rf '+ PATHRESTORETMP)    #######   Supression du repertoire temporaire    #######
+        os.system('rm -rf '+ PATHRESTORE)       #######   Supression du repertoire extraction temporaire    #######
 
         print('Restore databases + Wordpress + Apache is done')
 
@@ -99,19 +99,19 @@ def process_user_choice(user_choice):
         # Sauvegarde de la base
         if user_choice == 1:
 
-                print("Backup BDD + Wordpress + Apache")
+                print("Backup BDD + Wordpress + Apache")        #######   Choix utilisateur 1 = Sauvegarde    #######
                 backup()
 
         # Restauration de la base
         elif user_choice == 2:
-                print("Restore BDD + Wordpress + Apache")
+                print("Restore BDD + Wordpress + Apache")       #######   Choix utilisateur 2 = Restauration    #######
                 restore()
 
-        elif user_choice == 99:
+        elif user_choice == 99:                                 #######   Choix utilisateur 99 = Quite le programme    #######
                 exit(0)
 
         else:
-                sys.stderr.write("Error : Choice the number \n")
+                sys.stderr.write("Error : Choice the number \n")        #######   Si aucun des nombre du dessus message d'erreur    #######
                 sys.exit(1)
 
 
@@ -126,7 +126,7 @@ def main():
 
         while True:
                 try:
-                        user_choice = int(input("Enter your choice : > "))
+                        user_choice = int(input("Enter your choice : > ")) #######   Attente choix utilisateur     #######
                         break
                 except ValueError:
                         sys.stderr.write("Error : Undefined choice")
@@ -161,8 +161,8 @@ WP_PATH = (' /var/www')
 APACHE_PATH = (' /etc/apache2')
 
 try:
-        main()
-except KeyboardInterrupt:
+        main()                          #######   Démarage du programme     #######
+except KeyboardInterrupt:               #######   interuption     #######
         print('Process Interrupted')
         try:
                 sys.exit(0)
